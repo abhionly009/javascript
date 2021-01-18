8cd8f7581bf9035ecfe50d5199298e6a8abf19f0
abhi <abhionly009@gmail.com>
2021-01-18 10:29:06 +0530

    added code to give functionality to show original array on click of normal button'

--- sortArray.js
+++ sortArray.js
@@ -11,6 +11,18 @@ let actualArray = [ 'Jan',
     'Nov',
     'March'];
 
+
+/**
+ * used ES6 syntax for creating copy of array with value.(Spread operator [...name])
+ * benefit of copying value of if you are going to change anything into actual array it will 
+ * not affect your copy.
+ * 
+ * Note if you are using simple copy like this copyOfArray = actualArray;
+ * then it will copy the reference of original array not value, later if you are updating anything in original array it will impact 
+ * to your copy as well.
+ */
+let copyOfArray = [...actualArray];    
+
 /* 
     On load of window display all the data from array to list.
 */
@@ -18,6 +30,11 @@ window.onload = () => {
 
     actualArray.forEach(createDataAndAppend);
  }
+
+
+function createArraysData() {
+    copyOfArray.forEach(updateExistingList);
+}
 
 /* 
 create li node and append value to it and append to 
@@ -36,11 +53,7 @@ document.getElementById("asc").addEventListener('click', () => {
     actualArray.forEach(updateExistingList);
 })
 
-/**
- * 
- * @param {*} item 
- * @param {*} index 
- */
+
 function updateExistingList(item,index) {
     let months = document.getElementById('months');
     months.getElementsByTagName('li').item(index).innerText = item;
@@ -54,3 +67,9 @@ document.getElementById("dsc").addEventListener('click', () => {
     actualArray.forEach(updateExistingList);
 })
 
+/**
+ * add on click listener on normal button and display orinal array.
+ */
+document.getElementById('normal').addEventListener('click', () => {
+    createArraysData();
+})
